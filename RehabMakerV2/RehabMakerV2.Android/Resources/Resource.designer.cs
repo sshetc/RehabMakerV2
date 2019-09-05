0ea000e17ac639e8583a2e8b7a8ba77ca5d0190b
Алексей Белов <bj_sw@mail.ru>
2019-09-06 01:38:12 +0500

    Revert "huioh"
    This reverts commit 74243f155fdbc67ffe92c281630087ec278f1f5c.

--- RehabMakerV2/RehabMakerV2.Android/Resources/Resource.designer.cs
+++ RehabMakerV2/RehabMakerV2.Android/Resources/Resource.designer.cs
@@ -2342,29 +2342,29 @@ namespace RehabMakerV2.Droid
 			// aapt resource value: 0x7f020053
 			public const int avd_hide_password = 2130837587;
 			
+			// aapt resource value: 0x7f020136
+			public const int avd_hide_password_1 = 2130837814;
+			
 			// aapt resource value: 0x7f020137
-			public const int avd_hide_password_1 = 2130837815;
+			public const int avd_hide_password_2 = 2130837815;
 			
 			// aapt resource value: 0x7f020138
-			public const int avd_hide_password_2 = 2130837816;
-			
-			// aapt resource value: 0x7f020139
-			public const int avd_hide_password_3 = 2130837817;
+			public const int avd_hide_password_3 = 2130837816;
 			
 			// aapt resource value: 0x7f020054
 			public const int avd_show_password = 2130837588;
 			
+			// aapt resource value: 0x7f020139
+			public const int avd_show_password_1 = 2130837817;
+			
 			// aapt resource value: 0x7f02013a
-			public const int avd_show_password_1 = 2130837818;
+			public const int avd_show_password_2 = 2130837818;
 			
 			// aapt resource value: 0x7f02013b
-			public const int avd_show_password_2 = 2130837819;
-			
-			// aapt resource value: 0x7f02013c
-			public const int avd_show_password_3 = 2130837820;
+			public const int avd_show_password_3 = 2130837819;
 			
 			// aapt resource value: 0x7f020055
-			public const int Bletooth = 2130837589;
+			public const int bletooth = 2130837589;
 			
 			// aapt resource value: 0x7f020056
 			public const int brows = 2130837590;
@@ -3011,11 +3011,11 @@ namespace RehabMakerV2.Droid
 			// aapt resource value: 0x7f02012c
 			public const int notification_icon_background = 2130837804;
 			
-			// aapt resource value: 0x7f020135
-			public const int notification_template_icon_bg = 2130837813;
+			// aapt resource value: 0x7f020134
+			public const int notification_template_icon_bg = 2130837812;
 			
-			// aapt resource value: 0x7f020136
-			public const int notification_template_icon_low_bg = 2130837814;
+			// aapt resource value: 0x7f020135
+			public const int notification_template_icon_low_bg = 2130837813;
 			
 			// aapt resource value: 0x7f02012d
 			public const int notification_tile_bg = 2130837805;
@@ -3037,9 +3037,6 @@ namespace RehabMakerV2.Droid
 			
 			// aapt resource value: 0x7f020133
 			public const int tooltip_frame_light = 2130837811;
-			
-			// aapt resource value: 0x7f020134
-			public const int XamarinLogo = 2130837812;
 			
 			static Drawable()
 			{
